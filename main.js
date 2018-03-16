2cbb49952a64445159b0a3540b841428f1a2ea4f
James Kimani <mail4jameskimani@gmail.com>
2018-03-16 18:30:37 +0300

    added ipcmain 'saveIfWallpaper' event handler... saves images from Windows Spotlight folder to apps images folder... passing

--- main.js
+++ main.js
@@ -19,7 +19,7 @@
     
 
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 930, height: 525})
   
     // and load the index.html of the app.
     win.loadURL(url.format({
@@ -110,7 +110,7 @@
     var slFolder = store.get('app-folders.spotlight-folder')
     var imgsFolder = store.get('app-folders.images-folder')
     var imgsFolderFiles = fs.readdirSync(imgsFolder);
-   
+    
       Jimp.read(`${slFolder}/${file}`)
       .then(img => {
         var h = img.bitmap.height;
